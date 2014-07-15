b276eaeef854a7519c86443cedc5eec9724c7eda
gf0842wf <gf0842wf@gmail.com>
2014-07-15 18:56:19 +0800

    example
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from server import RPCServer
-from client import Pool
+from client import Connection, Pool
 from cPickle import dumps, loads
 import gevent
 
@@ -12,14 +12,23 @@ class TestRPCServer(RPCServer):
     
     def RPC_add(self, a, b):
         return a + b
+
+    def RPC_sleep(self, seconds):
+    	gevent.sleep(seconds)
     
 
 if __name__ == "__main__":
     TestRPCServer(7000).start()
     
+    c = Connection(("127.0.0.1", 7000), dumps, loads)
+    print c.RPC_sleep(3) 
+    c.reconnect() # Connection 是短连接,必须主动重连才能继续利用 connection
+    print c.RPC_add(2, 3)
+    
     args = (("127.0.0.1", 7000), dumps, loads)
     p = Pool(args, 6)
-    print p.RPC_add(2, 3)
+    print p.RPC_echo("123")
+    print p.RPC_add(1, 2) # Pool 是长连接,不需要重连
     # [0, 3] 其中 0-表示成功, 3-结果值
     
     gevent.wait()
