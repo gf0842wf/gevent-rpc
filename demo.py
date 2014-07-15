135ee31fe3cf3cdd75ead01eb31a161ef9a24129
gf0842wf <gf0842wf@gmail.com>
2014-07-15 09:47:55 +0800

    example
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

---
+++ demo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from server import RPCServer
+from client import Pool
+from cPickle import dumps, loads
+import gevent
+
+
+class TestRPCServer(RPCServer):
+    
+    def RPC_add(self, l, r):
+        return l + r
+    
+
+if __name__ == "__main__":
+    TestRPCServer(7000).start()
+    
+    args = (("127.0.0.1", 7000), dumps, loads)
+    p = Pool(args, 6)
+    print p.RPC_add(1, 2)
+    
+    gevent.wait()
