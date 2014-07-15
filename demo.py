836617519875c14027222f6bc9656186311d7393
gf0842wf <gf0842wf@gmail.com>
2014-07-15 13:55:16 +0800

    example
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- demo.py
+++ demo.py
@@ -10,8 +10,8 @@ class TestRPCServer(RPCServer):
     dumps = dumps
     loads = loads
     
-    def RPC_add(self, l, r):
-        return l + r
+    def RPC_add(self, a, b):
+        return a + b
     
 
 if __name__ == "__main__":
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     
     args = (("127.0.0.1", 7000), dumps, loads)
     p = Pool(args, 6)
-    print p.RPC_add(1, 2)
+    print p.RPC_add(2, 3)
     # [0, 3] 其中 0-表示成功, 3-结果值
     
     gevent.wait()
